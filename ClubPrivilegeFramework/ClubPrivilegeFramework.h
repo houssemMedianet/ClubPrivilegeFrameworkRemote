5363751b11ba01dc3ff5408d0c0cd8f9cf34b6bb
HOUSSEM <houssem.bensiksalem@medianet.com.tn>
2023-01-03 16:19:12 +0100

    Initial Commit

---
+++ ClubPrivilegeFramework/ClubPrivilegeFramework.h
@@ -0,0 +1,18 @@
+//
+//  ClubPrivilegeFramework.h
+//  ClubPrivilegeFramework
+//
+//  Created by houssem.bensiksalem on 3/1/2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ClubPrivilegeFramework.
+FOUNDATION_EXPORT double ClubPrivilegeFrameworkVersionNumber;
+
+//! Project version string for ClubPrivilegeFramework.
+FOUNDATION_EXPORT const unsigned char ClubPrivilegeFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ClubPrivilegeFramework/PublicHeader.h>
+
+
